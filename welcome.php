0a72ebe5adefc99caf7282ebd6e228d6dd5f7d7c
Anirudh dhuhan <anirudh.duhoon@gmail.com>
2023-04-04 11:28:07 +0530

    welcome and function added

---
+++ welcome.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?>!<br>
+Your email address is: <?php echo $_POST["email"]; ?> <br>
+Website is: <?php echo $_POST["website"]; ?><br>
+Comment: <?php echo $_POST["comment"]; ?><br>
+Gender: <?php echo $_POST["gender"]; ?><br>
+</body>
+</html> <br>
+
+
+<?php
+//echo '<pre>';
+//print_r($_POST);
+// die;
+?>
