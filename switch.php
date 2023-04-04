b2bd977c2f938b4d5ccceede3988bb33d145aaf4
Anirudh dhuhan <anirudh.duhoon@gmail.com>
2023-04-04 11:23:10 +0530

    switch added

---
+++ switch.php
@@ -0,0 +1,13 @@
+<?php
+$favcar= "Bmw";
+Switch($favcar){
+	case "bmw":
+	echo "Your favorite colour is red";
+	break;
+	case "Mercedes";
+	echo "Your favorite car is Mercedes";
+	break;
+	default:
+	echo "None of them is your favorite car";
+}
+?>
